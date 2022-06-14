c7952dcd84184b4c7af94440eea2097916ab107d
Tanzim Hossain <tanzim35-2700@diu.edu.bd>
2022-06-14 17:18:34 +0600

    calculator added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_02/page/calculator_page.dart';
 
-import 'home_page.dart';
+import 'page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(msg: 'Currency Converter',),
+      home: CalculatorPage(),
     );
   }
 
